7cfaa2484ac68e7b356f40f94e011a55e121d97d
shanejohnson <shane.john316@gmail.com>
2023-12-16 14:14:33 -0600

    Added css styles to the index and highScore pages

--- assets/js/quizLogicScript.js
+++ assets/js/quizLogicScript.js
@@ -1,38 +1,103 @@
 const questions = [
     {
         question: 'JavaScript is an _______ language',
-        answers: ['Object-Oriented', 'Object-Based', 'Procedural', 'None of the above'],
-        correctAnswer: 'Object-Oriented'
+        answers: ['A. Object-Oriented', 'B. Object-Based', 'C. Procedural', 'D. None of the above'],
+        correctAnswer: 'A. Object-Oriented'
     },
     {
         question: 'Which of the following keywords is used to define a variable in JavaScript?',
-        answers: ['var', 'let', 'const', 'All of the above'],
-        correctAnswer: 'All of the above'
+        answers: ['A. var', 'B. let', 'C. const', 'D. All of the above'],
+        correctAnswer: 'D. All of the above'
     },
     {
         question: 'Which of the following methods is used to access HTML elements using Javascript?',
-        answers: ['getElementById()', 'getElementByClassName()', 'Both A and B', 'None of the above'],
-        correctAnswer: 'Both A and B'
+        answers: ['A. getElementById()', 'B. getElementByClassName()', 'C. Both A and B', 'D. None of the above'],
+        correctAnswer: 'C. Both A and B'
     },
     {
         question: 'How can a datatype be declared to be a constant type?',
-        answers: ['const', 'var', 'let', 'constant'],
-        correctAnswer: 'const'
+        answers: ['A. const', 'B. var', 'C. let', 'D. constant'],
+        correctAnswer: 'A. const'
     },
     {
         question: 'Using a(n) _______ statement is how you test for a specific condition.',
-        answers: ['Select', 'If', 'Switch', 'For'],
-        correctAnswer: 'If'
+        answers: ['A. Select', 'B. If', 'C. Switch', 'D. For'],
+        correctAnswer: 'B. If'
     },
     {
         question: 'How do you write "Hello World" in an alert box?',
-        answers: ['alertBox("Hello World");', 'msgBox("Hello World");', 'msg("Hello World");', 'alert("Hello World");'],
-        correctAnswer: 'alert("Hello World");'
+        answers: ['A. alertBox("Hello World");', 'B. msgBox("Hello World");', 'C. msg("Hello World");', 'D. alert("Hello World");'],
+        correctAnswer: 'D. alert("Hello World");'
     },
     {
         question: 'How do you create a function in JavaScript?',
-        answers: ['const myFunction = function()', 'function myFunction()', 'const myFunction = () =>', 'All of the above'],
-        correctAnswer: 'All of the above'
+        answers: ['A. const myFunction = function()', 'B. function myFunction()', 'C. const myFunction = () =>', 'D. All of the above'],
+        correctAnswer: 'D. All of the above'
+    },
+    {
+        question: 'Which of the following is NOT a primitive data type in JavaScript?',
+        answers: ['A. Number', 'B. String', 'C. Boolean', 'D. Object'],
+        correctAnswer: 'D. Object'
+    },
+    {
+        question: 'What is the output of the following code: console.log(2 + "2");',
+        answers: ['A. "4"', 'B. "22"', 'C. 4', 'D. 22'],
+        correctAnswer: 'B. "22"'
+    },
+    {
+        question: 'Which of the following is NOT a comparison operator in JavaScript?',
+        answers: ['A. ==', 'B. ===', 'C. !=', 'D. =<'],
+        correctAnswer: 'D. =<'
+    },
+    {
+        question: 'What does the "NaN" value represent in JavaScript?',
+        answers: ['A. Not a number', 'B. Null value', 'C. Undefined value', 'D. Boolean value'],
+        correctAnswer: 'A. Not a number'
+    },
+    {
+        question: 'What is the output of the following code: console.log(2 ** 3);',
+        answers: ['A. 5', 'B. 6', 'C. 8', 'D. 9'],
+        correctAnswer: 'C. 8'
+    },
+    {
+        question: 'What is the correct syntax for a "for" loop in JavaScript?',
+        answers: ['A. for (var i = 0; i < 5; i++)', 'B. for(i = 0; i < 5; i++)', 'C. for (var i = 5; i > 0; i-)', 'D. for (i = 5; i > 0; i-)'],
+        correctAnswer: 'A. for (var i = 0; i < 5; i++)'
+    },
+    {
+        question: 'Which of the following is not a data type in JavaScript?',
+        answers: ['A. Boolean', 'B. String', 'C. Number', 'D. Character'],
+        correctAnswer: 'D. Character'
+    },
+    {
+        question: 'What is the output of the following code: console.log("5" == 5);',
+        answers: ['A. true', 'B. false', 'C. Error', 'D. Nan'],
+        correctAnswer: 'A. true'
+    },
+    {
+        question: 'What is the output of the following code: console.log(2 + 3 + "4");',
+        answers: ['A. "54"', 'B. "9"', 'C. "234"', 'D. Error'],
+        correctAnswer: 'A. "54"'
+    },
+    {
+        question: 'What is the output of the following code: console.log(type of NaN);',
+        answers: ['A. "number"', 'B. "string"', 'C. "undefined"', 'D. "NaN"'],
+        correctAnswer: 'A. "number"'
+    },
+    {
+        question: 'What is the output of the following code: console.log(typeof []);',
+        answers: ['A. "array"', 'B. "object"', 'C. "array[]"', 'D. "undefined"'],
+        correctAnswer: 'B. "object"'
+    },
+    {
+        question: 'Which of the following is not a valid way to declare a function in JavaScript?',
+        answers: ['A. function myFunction() {}', 'B. var myFunction = function() {}', 'C. () => {}', 'D. function = {}'],
+        correctAnswer: 'D. function = {}'
+    },
+    {
+        question: 'Which of the following is not a loop in JavaScript?',
+        answers: ['A. for', 'B. while', 'C. do...while', 'D. next'],
+        correctAnswer: 'D. next'
     }
 ]
 
@@ -46,6 +111,7 @@ const answerButtons = document.querySelectorAll('.answer-button');
 const feedback = document.querySelector('h2');
 const initialsForm = document.querySelector('#enter-initials');
 let timerInterval;
+let remainingTime;
 
 let score = 0;
 
@@ -65,11 +131,17 @@ startButton.addEventListener('click', () => {
 });
 
 function startTimeCountdown() {
-    let remainingTime = 60;
+    remainingTime = 120;
     timerElement.innerText = remainingTime;
     timerInterval = setInterval(() => {
         remainingTime -= 1;
         timerElement.innerText = remainingTime;
+        if (remainingTime < 60) {
+            timerElement.setAttribute('style', 'color: red');
+        }
+        else {
+            timerElement.setAttribute('style', 'color: black');
+        }
         if (remainingTime === 0) {
             clearInterval(timerInterval);
             gameOver();
@@ -104,6 +176,8 @@ function handleAnswerClick(event) {
     else {
         feedback.innerText = 'Wrong';
         feedback.style.color = 'red';
+        remainingTime -= 10;
+        timerElement.setAttribute('style', 'color: red');
     }
     setTimeout(() => {
         if (questions.length === 0) {
